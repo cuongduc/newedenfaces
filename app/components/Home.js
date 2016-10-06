ffa91bae3130964ee7af37b413e82b7ff800d7a9
Duc Cuong <duccuong.hust@gmail.com>
2016-10-06 18:47:04 +0700

    Finish project settings

---
+++ app/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div className='alert alert-info'>
+        Hello from Home Component
+      </div>
+    );
+  }
+}
+
+export default Home;
