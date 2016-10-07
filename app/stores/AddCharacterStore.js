13969bf9d018006e746d8bcb1469da454b5d9c58
Duc Cuong <duccuong.hust@gmail.com>
2016-10-07 08:36:03 +0700

    Finish Character component

---
+++ app/stores/AddCharacterStore.js
@@ -0,0 +1,47 @@
+import alt from '../alt';
+import AddCharacterActions from '../actions/AddCharacterActions';
+
+
+class AddCharacterStore {
+  constructor() {
+    this.bindActions(AddCharacterActions);
+    this.name = '';
+    this.gender = '';
+    this.helpBlock = '';
+    this.nameValidationState = '';
+    this.genderValidationState = '';
+  }
+
+  onAddCharacterSuccess(successMessage) {
+    this.nameValidationState = 'has-success';
+    this.helpBlock = successMessage
+  }
+
+  onAddCharacterFail(errorMessage) {
+    this.nameValidationState = 'has-error';
+    this.helpBlock = errorMessage;
+  }
+
+  onUpdateName(event) {
+    this.name = event.target.value;
+    this.nameValidationState = '';
+    this.helpBlock = '';
+  }
+
+  onUpdateGender(event) {
+    this.gender = event.target.value;
+    this.genderValidationState = '';
+    this.helpBlock = '';
+  }
+
+  onInvalidName() {
+    this.nameValidationState = 'has-error';
+    this.helpBlock = 'Please enter a character name';
+  }
+
+  onInvalidGender() {
+    this.genderValidationState = 'has-error';
+  }
+}
+
+export default alt.createStore(AddCharacterStore);
